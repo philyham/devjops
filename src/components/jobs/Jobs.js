e2056282d735367e2761b693cbb52dc8a71c6971
phillybebe <philia.hammond@eggseed.com>
2023-09-08 21:13:18 +0000

    styling components

--- src/components/jobs/Jobs.js
+++ src/components/jobs/Jobs.js
@@ -1,33 +1,35 @@
 import React from 'react'
 import './jobs.css'
 import DataJobs from "../../data/data.json"
+import { BsDot } from "react-icons/bs"
 
 const Jobs = () => {
   return (
-      <>
+      <div className="wrapper">
       {DataJobs.map((item, id ) => {
         return(
-          <div className="wrapper">
-            <div>
+          <div>
+            <div className="logo">
               <img src={item.logo} alt="company logo" />
             </div>
-           <div>
+           <div className="time">
             <p>{item.postedAt}</p>
+             <BsDot size={25} className="icon"/>
             <p>{item.contract}</p>
           </div>
           <div>
-            <h2>{item.position}</h2>
-            <h4>{item.description}</h4>
+            <h2 className="title">{item.position}</h2>
+            {/* <p lassName="description">{item.description}</p> */}
           </div>
-          <div>
-            <a href="">{item.website}</a>
-            <a href="">{item.apply}</a>
-            <p>{item.location}</p>
+          <div className="links">
+          <a href={item.website} className="website"><p>{item.company}</p></a>
+          {/* <a href={item.apply}><p>{item.apply}</p></a> */}
+          <a href="" className="location"><p>{item.location}</p></a>
           </div>
           </div>
         )
       })}
-      </>
+      </div>
     )
 }
 
